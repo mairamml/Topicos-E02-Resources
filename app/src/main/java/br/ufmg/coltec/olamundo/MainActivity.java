719cb952a3299764b0c0ffd15da2426aed8bae17
mairamml <mairaaml11@gmail.com>
2024-08-20 12:12:35 -0300

    Atividade 1 - Externalizando strings

--- app/src/main/java/br/ufmg/coltec/olamundo/MainActivity.java
+++ app/src/main/java/br/ufmg/coltec/olamundo/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 //                TODO(#1): Esse texto não deveria estar aqui
-                Toast toast = Toast.makeText(MainActivity.this, "Bem vindo, mestre", Toast.LENGTH_LONG);
+                Toast toast = Toast.makeText(MainActivity.this, R.string.welcomeStr, Toast.LENGTH_LONG);
                 toast.setGravity(CENTER, 0, 0);
                 toast.show();
             }
